a07a4ed7d58a8133fbff6396122f96082f549eaa
Dixontirtayadi <Dixontirtayadi@gmail.com>
2019-12-23 23:04:16 -0800

    added the form and client side javascript

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,31 @@
 console.log("Client side javascript file");
+
+const weatherForm = document.querySelector("form")
+const userInput = document.querySelector("input")
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const location = userInput.value
+
+    const messageOne = document.querySelector("#message-1")
+    const messageTwo = document.querySelector("#message-2")
+    
+    if (location === "") {
+        return messageOne.textContent = "Type in an address"
+    }
+
+    messageOne.textContent = "Loading..."
+    messageTwo.textContent = ""
+
+    fetch( "http://localhost:3000/weather?address=" + encodeURIComponent(location) ).then((response) => {
+        response.json().then( (data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = "Forecast on " + data.location
+                messageTwo.textContent = data.forecastData
+            }
+        })
+    })
+})
